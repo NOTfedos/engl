1305a05d09ae9d9a8d72c64dee36d2e959286604
NOTfedos <mistkap@gmail.com>
2021-05-11 19:45:35 +0300

    first commit

---
+++ voc_prog.py
@@ -0,0 +1,74 @@
+from random import randint, sample
+
+f = open("vocab_dict_tr.txt", "r", encoding="utf-8")
+
+words = dict()
+rest = dict()
+for line in f:
+    if line[0] != "-":
+        eng, rus = line.replace("\n", "").split("; ")
+        eng = eng.strip()
+        rus = rus.strip()
+        words.update({eng: rus})
+
+print("Data loaded, starts exam")
+
+k = 0
+right = 0
+k_end = len(words.keys())
+
+for eng, rus in sample(words.items(), k_end):
+    k += 1
+    print(f"--------------------------- {k} / {k_end} ------------------------------------")
+    if randint(0, 1) == 0:
+        # print(f"{k} / {k_end}: {rus}, your translate is ", end='')
+        ans = input(f"{k} ({right} / {k_end}): {rus}, your translate is ")
+        if ans.lower() == eng.lower():
+            right += 1
+            print("RIGHT")
+        else:
+            rest.update({eng: rus})
+            print(f"WRONG, translate is {eng}")
+    else:
+        # print(f"{k} / {k_end}: {eng}, your translate is ", end='')
+        ans = input(f"{k} ({right} / {k_end}): {eng}, your translate is ")
+        if ans.lower() == rus.lower():
+            right += 1
+            print("RIGHT")
+        else:
+            rest.update({eng: rus})
+            print(f"WRONG, translate is {rus}")
+
+print("--------------------------------------------------------------------")
+print("Now lest deal with remain words")
+
+while len(rest.keys()) != 0:
+    k = 0
+    right = 0
+    k_end = len(rest.keys())
+    print("---------------------------RESOLVING-------------------------------------")
+    for eng, rus in rest.items():
+        k += 1
+        print(f"--------------------------- {k} / {k_end} ------------------------------------")
+        if randint(0, 1) == 0:
+            # print(f"{k} / {k_end}: {rus}, your translate is ", end='')
+            ans = input(f"{k} ({right} / {k_end}): {rus}, your translate is ")
+            if ans.lower() == eng.lower():
+                right += 1
+                print("RIGHT")
+                rest.pop(eng)
+            else:
+                rest.update({eng: rus})
+                print(f"WRONG, translate is {eng}")
+        else:
+            # print(f"{k} / {k_end}: {eng}, your translate is ", end='')
+            ans = input(f"{k} ({right} / {k_end}): {eng}, your translate is ")
+            if ans.lower() == rus.lower():
+                right += 1
+                print("RIGHT")
+                rest.pop(eng)
+            else:
+                rest.update({eng: rus})
+                print(f"WRONG, translate is {rus}")
+
+print("That's all")
